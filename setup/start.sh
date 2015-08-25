dfd82e807ce4c59996473390fa70c5330979a007
Dave Webb <github@d5ve.com>
2015-08-26 10:52:37 +1200

    Comment out owncloud setup in start.sh - it's broken for me and I don't use it.

--- setup/start.sh
+++ setup/start.sh
@@ -101,7 +101,7 @@ source setup/dkim.sh
 source setup/spamassassin.sh
 source setup/web.sh
 source setup/webmail.sh
-source setup/owncloud.sh
+#Broken for me, so skip# source setup/owncloud.sh
 source setup/zpush.sh
 source setup/management.sh
 source setup/munin.sh
